5d93c7d1fcdc03b93b771d139d28f65e5b5b58ac
chancemusselman <musselman.chance@icloud.com>
2018-10-02 19:53:07 -0700

    First Draft of Main Page Layout/Style TODO: ALL LOGIC (Both All User Items & Dynamic Content Ggeneration) TODO: UserProfiles TODO: ChatViewWindow TODO: Settings TODO: LOGIN/INTRO Page TODO: SWIPE View TODO: Review All Mobile Views

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import API from "./utils/API";
+import Container from "./components/container/index";
+
 
 class App extends Component {
-  state = {
-    text: "test"
-  }
-  componentDidMount() {
-    API.getTest()
-      .then(res => {
-        this.setState({ text: res.data })
-      })
-      .catch(err => console.log(err));
-    // API.postTest()
-    //   .then(res => {
-    //     this.setState({ text: res.data })
-    //   })
-    //   .catch(err => console.log(err));
-  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          {this.state.text}
-        </p>
-      </div>
+
+      <Container />
+
     );
   }
 }
