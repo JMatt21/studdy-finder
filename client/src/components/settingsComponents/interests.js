79343155006bd0cf686dddbed85113169329e018
Bob Ross <ministash14@gmail.com>
2018-10-10 11:56:31 -0700

    Added some animations to the settings page

--- client/src/components/settingsComponents/interests.js
+++ client/src/components/settingsComponents/interests.js
@@ -1,11 +1,99 @@
 import React from "react";
-import "./settingsComponents.css";
+import "./profilePicture.css";
+import head from '../../assets/head.jpg'
+import { Animated } from "react-animated-css";
 
-const interests = () => (
-  <div>
-    Swift
-  </div>
-);
+class ProfilePicture extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newClass: "profile-picture-wrapper-1"
+    };
 
-export default interests;
+  }
 
+
+  changeBackgroundInput = (e) => {
+    e.preventDefault();
+
+    if (this.state.newClass === "profile-picture-wrapper-1") {
+      this.setState({
+        newClass: "profile-picture-wrapper-2"
+      });
+
+    }
+  }
+
+  changeBackground = (e) => {
+    e.preventDefault();
+
+    if (this.state.newClass === "profile-picture-wrapper-2") {
+      this.setState({
+        newClass: "profile-picture-wrapper-1"
+      });
+
+    }
+
+  }
+
+
+
+  render() {
+
+
+
+
+
+    return (
+      <div onClick={this.changeBackground} className={this.state.newClass}>
+
+        <div></div>
+
+        <div className="settings-profile-picture-wrapper">
+          <div className="settings-profile-picture">
+            <img src={head} width="250px" height="250px" />
+          </div>
+          <Animated animationIn="fadeIn">
+            <h4 className="display-text-settings-profile-picture bounceIn">Enter A link To Your New Profile Picture Here</h4>
+            <p className="display-text-settings-profile-picture">
+              (We recommond that your profile picture is 250px x 250px to ensure proper sizing)</p>
+          </Animated>
+
+        </div>
+
+
+        <div></div>
+        <div></div>
+
+
+
+        <div className="settings-profile-link">
+          <form className="settings-link-form-wrapper">
+            <div className="bubble">
+              <div id="messaging-form">
+                <div className="input-field">
+                  <input onClick={this.changeBackgroundInput}
+                    type="text"
+                    className="materialize-textarea"
+                    name="message"
+                    placeholder="Enter A New Link Here" />
+                </div>
+              </div>
+            </div>
+            <button className="circleButton" onClick={this.changeBackground}>Submit</button>
+          </form>
+
+        </div>
+
+        <div></div>
+
+      </div>
+
+
+    );
+  }
+}
+
+
+
+export default ProfilePicture;
