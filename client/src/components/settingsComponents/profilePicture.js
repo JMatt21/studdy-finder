e7a9ded8e442759506b1ab4a9d15f67dea5f021b
Bob Ross <ministash14@gmail.com>
2018-10-09 21:20:49 -0700

    Safety Commit

--- client/src/components/settingsComponents/profilePicture.js
+++ client/src/components/settingsComponents/profilePicture.js
@@ -1,15 +1,38 @@
 import React from "react";
 import "./settingsComponents.css";
+import head from '../../assets/head.jpg'
 
 const profilePicture = () => (
   <div className="profile-picture-wrapper">
     <div></div>
-    <div className="settings-profile-picture">Top Stuff</div>
+    <div className="settings-profile-picture-wrapper">
+      <img src={head} className="settings-profile-picture" />
+    </div>
     <div></div>
     <div></div>
-    <div className="settings-profile-link">Content</div>
+    <div className="settings-profile-link">
+      <form>
+        <div className="bubble">
+          <div id="messaging-form">
+            <div className="input-field">
+              <input type="text"
+                className="materialize-textarea"
+                name="message"
+                placeholder="Enter A New Link Here"
+              />
+            </div>
+          </div>
+        </div>
+        <button className="circleButton" onClick={this.mSending}>Submit</button>
+      </form>
+
+
+
+
+
+    </div>
     <div></div>
-    
+
   </div>
 );
 
