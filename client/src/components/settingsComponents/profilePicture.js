70171b8851e8f3ff6b38703b5a053eda2b9f6adc
Bob Ross <ministash14@gmail.com>
2018-10-09 20:53:59 -0700

    Making the picture page

--- client/src/components/settingsComponents/profilePicture.js
+++ client/src/components/settingsComponents/profilePicture.js
@@ -2,8 +2,14 @@ import React from "react";
 import "./settingsComponents.css";
 
 const profilePicture = () => (
-  <div>
-    Hunter
+  <div className="profile-picture-wrapper">
+    <div></div>
+    <div className="settings-profile-picture">Top Stuff</div>
+    <div></div>
+    <div></div>
+    <div className="settings-profile-link">Content</div>
+    <div></div>
+    
   </div>
 );
 
---
+++ client/src/components/settingsComponents/username_password.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./settingsComponents.css";
+
+const username_password = () => (
+  <div>
+    Hunter
+  </div>
+);
+
+export default username_password;
