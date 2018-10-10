b7e6da09f44829fabff0165f561b5b067f657676
Bob Ross <ministash14@gmail.com>
2018-10-10 09:51:19 -0700

    Finished the styling with the profile picture page, just need to add logic

--- client/src/components/settingsComponents/profilePicture.js
+++ client/src/components/settingsComponents/profilePicture.js
@@ -2,38 +2,62 @@ import React from "react";
 import "./settingsComponents.css";
 import head from '../../assets/head.jpg'
 
-const profilePicture = () => (
-  <div className="profile-picture-wrapper">
-    <div></div>
-    <div className="settings-profile-picture-wrapper">
-      <img src={head} className="settings-profile-picture" />
-    </div>
-    <div></div>
-    <div></div>
-    <div className="settings-profile-link">
-      <form>
-        <div className="bubble">
-          <div id="messaging-form">
-            <div className="input-field">
-              <input type="text"
-                className="materialize-textarea"
-                name="message"
-                placeholder="Enter A New Link Here"
-              />
-            </div>
+class ProfilePicture extends React.Component {
+
+  render() {
+    return (
+      <div className="profile-picture-wrapper">
+
+        <div></div>
+
+        <div className="settings-profile-picture-wrapper">
+          <div className="settings-profile-picture">
+            <img src={head} width="250px" height="250px" />
           </div>
+          <h4 className="display-text-settings-profile-picture">Enter A link To Your New Profile Picture Here</h4>
+          <p className="display-text-settings-profile-picture">
+            (We recommond that your profile picture is 250px x 250px to ensure proper sizing)</p>
+
         </div>
-        <button className="circleButton" onClick={this.mSending}>Submit</button>
-      </form>
+
+
+        <div></div>
+        <div></div>
+
+
+
+        <div className="settings-profile-link">
+          <form className="settings-link-form-wrapper">
+            <div className="bubble">
+              <div id="messaging-form">
+                <div className="input-field">
+                  <input type="text"
+                    className="materialize-textarea"
+                    name="message"
+                    placeholder="Enter A New Link Here" />
+                </div>
+              </div>
+            </div>
+            <button className="circleButton">Submit</button>
+          </form>
+
+
+
+        </div>
+
+        <div></div>
 
 
 
 
 
-    </div>
-    <div></div>
+      </div>
 
-  </div>
-);
 
-export default profilePicture;
+    );
+  }
+}
+
+
+
+export default ProfilePicture;
