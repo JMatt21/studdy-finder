10bd8ad28933f56a549793424e747ab703c7d3a4
chancemusselman <musselman.chance@icloud.com>
2018-10-15 00:49:45 -0700

    Nav-Bar Responsivness Fixed In Settings & Passport Pages | TODO: Integrate Ashley's Login/Signup pages, Message Board Button DropDown, Settings Page Responsivness

--- client/src/components/passportpages/Members.js
+++ client/src/components/passportpages/Members.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./passportPages.css";
 import API from "../../utils/PassportAPI";
 
 export class Members extends React.Component {
@@ -8,8 +9,8 @@ export class Members extends React.Component {
 
     componentWillMount() {
         API.getUserInfo()
-            .then(({data}) => {
-                console.log(data); 
+            .then(({ data }) => {
+                console.log(data);
                 this.setState({
                     email: data.email
                 })
@@ -33,10 +34,17 @@ export class Members extends React.Component {
                         </div>
                     </div>
                 </nav>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6 col-md-offset-3">
-                            <h2>Hi <span class="member-name">{this.state.email}</span></h2>
+                <div className="passport-grid-container">
+                    <div className="nav-passport-area">
+
+                    </div>
+                    <div className="main-passport-display">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-6 col-md-offset-3">
+                                    <h2>Hi <span class="member-name">{this.state.email}</span></h2>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
