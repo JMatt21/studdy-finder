57bce49f6334bc4cbec7ca92975be558f03c134c
chancemusselman <musselman.chance@icloud.com>
2018-10-15 02:42:47 -0700

    Changed Class Names in (Message Board) component to be more uniform with the rest of the app

--- client/src/components/messageBoard/messageBoard.js
+++ client/src/components/messageBoard/messageBoard.js
@@ -83,9 +83,9 @@ class MessageBoard extends React.Component {
 
     render() {
         return (
-            <div className="messageBoardWrapper">
+            <div className="message-board-wrapper">
                 <div className="spacer"></div>
-                <h5 className="messageBoardHeader ubuntu">Message Center</h5>
+                <h5 className="message-board-header ubuntu">Message Center</h5>
                 {/* <div className="alert">3</div> */}
                 <MessageCard
                     {...this.props}
