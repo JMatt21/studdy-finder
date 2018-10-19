9aec2ce641ec8f2e0d159e070f31d71817d3518b
JustMatt <MatthewBertrandt@gmail.com>
2018-10-18 18:14:07 -0700

    weird error in login

--- client/src/components/passportpages/Login.js
+++ client/src/components/passportpages/Login.js
@@ -15,7 +15,7 @@ export class Login extends React.Component {
         // The parameter is all the props being pass through
         console.log("APPSTATE WAS UPDATED")
         if (props.appState.user.id) {
-            
+
             API.searchForUsers(props.appState.user.beginnerSkills, props.appState.user.latitude, props.appState.user.longitude, 100000000, props.appState.user.id)
                 .then(({ data }) => {
                     this.props.setData(data, 'carousel');
@@ -58,55 +58,55 @@ export class Login extends React.Component {
             <div className="limiter">
                 <div className="container-login100">
                     <div className="wrap-login100">
-                    <Animated animationIn="zoomInUp">
-                        <form className="login100-form validate-form">
+                        <Animated animationIn="zoomInUp">
+                            <form className="login100-form validate-form">
 
-                            <h4 className="lsTitle login100-form-title p-b-34 p-t-27">
-                                Study-Duo
+                                <h4 className="lsTitle login100-form-title p-b-34 p-t-27">
+                                    Study-Duo
                             </h4>
 
-                            <span className="login100-form-logo">
-                                <img className="zmdi zmdi-landscape" src={logo} alt="logo"></img>
-                            </span>
+                                <span className="login100-form-logo">
+                                    <img className="zmdi zmdi-landscape" src={logo} alt="logo"></img>
+                                </span>
 
-                            <span className="login100-form-title p-b-34 p-t-27">
-                                Log In
+                                <span className="login100-form-title p-b-34 p-t-27">
+                                    Log In
                         </span>
 
-                            <div className="wrap-input100 validate-input" data-validate="Enter username">
-                                <input className="input100"
-                                    type="email"
-                                    name="email"
-                                    value={this.state.email}
-                                    onChange={this.handleInputChange}
-                                    placeholder="Email" />
-                            </div>
+                                <div className="wrap-input100 validate-input" data-validate="Enter username">
+                                    <input className="input100"
+                                        type="email"
+                                        name="email"
+                                        value={this.state.email}
+                                        onChange={this.handleInputChange}
+                                        placeholder="Email" />
+                                </div>
 
-                            <div className="wrap-input100 validate-input" data-validate="Enter password">
-                                <input className="input100"
-                                    type="password"
-                                    name="password"
-                                    value={this.state.password}
-                                    onChange={this.handleInputChange}
-                                    placeholder="Password" />
-                            </div>
-                            <div className="login-signup-wrapper">
-                                <div className="container-login100-form-btn">
-                                    <button className="login100-form-btn" type="submit" onClick={this.handleFormSubmission}>
-                                        Log In
+                                <div className="wrap-input100 validate-input" data-validate="Enter password">
+                                    <input className="input100"
+                                        type="password"
+                                        name="password"
+                                        value={this.state.password}
+                                        onChange={this.handleInputChange}
+                                        placeholder="Password" />
+                                </div>
+                                <div className="login-signup-wrapper">
+                                    <div className="container-login100-form-btn">
+                                        <button className="login100-form-btn" type="submit" onClick={this.handleFormSubmission}>
+                                            Log In
                             </button>
-                                </div>
-                                <div className="container-login100-form-btn">
-                                <Link to="/signup" className="login100-form-btn">
-                                        Sign Up
+                                    </div>
+                                    <div className="container-login100-form-btn">
+                                        <Link to="/signup" className="login100-form-btn">
+                                            Sign Up
                                 </Link>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
                         </Animated>
                     </div>
                 </div>
-            </div>
-        </div>)
+            </div>)
+        
     }
 };
